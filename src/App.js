9ed851147d71cb55a3f49ae560b94bf0aa4cb902
Pedro Reis <preis@zak.app>
2021-10-01 22:23:45 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={require('./assets/img/cadin-logo.png').default} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Cadin boilerplate! Wish us good luck.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          By Afonso, Amanda, Pedro and Vitor.
+        </p>
       </header>
     </div>
   );
