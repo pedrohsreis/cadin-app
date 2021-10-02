9fe6042ffb565f7758b232c241879710577b1d25
Pedro Reis <preis@zak.app>
2021-10-02 10:05:45 -0300

    feat: map with navbar, positional markers and search input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={require('../../assets/img/cadin-logo.png').default} />
         <p>
           Welcome to the Cadin mapping app! Wish us good luck.
