c9ca8eaed72785dfe257ca4cebd882f3e2f3ff1c
Afonso Braga <afonso.braga@engenharia.ufjf.br>
2021-10-03 19:52:24 -0300

    edit Readme

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -29,13 +29,6 @@ export default class Tools extends Component {
 
     static pushpins = [
         require("../assets/img/img_verm.png"),
-        // "https://lh3.googleusercontent.com/ogw/ADea4I4IXxCNWS5_ZXwkGibaAyMkq_x2wXzu1Keq0GEb=s32-c-mo",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-blue.png",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-orange.png",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-teal.png",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-purple.png",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-white.png",
-        // "https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-black.png"
     ];
 
     constructor(props) {
